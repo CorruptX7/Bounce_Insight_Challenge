6b00a4ea6dd6b821d4e2a98b7fdea243f5a7c782
Cheuk Wei <cheukwei777@gmail.com>
2023-10-26 10:02:06 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,15 @@
+## Description
+
+This website allows users to search for a specific country. When a user makes a search, the website retrieves data from the back-end server. Then, it presents the relevant information about the searched country on the front-end, which is built using React.
+
+## Back-end pre-requisites
+
+Make sure you cd into the /server directory and to start the server run:
+
+- npm run dev
+
+## Front-end pre-requisites
+
+Make sure you cd into the /client directory and to start the client run:
+
+- npm start
---
+++ server/server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const axios = require('axios')
+const app = express()
+
+app.get('/country', async (req, res) => {
+  try {
+    // define country variable
+    const { country } = req.query
+
+    if (!country) {
+      return res.status(400).json({ error: 'Missing country parameter' })
+    }
+
+    const response = await axios.get(
+      `https://restcountries.com/v3.1/name/${country}`
+    )
+    res.json(response.data)
+  } catch (error) {
+    console.error(error)
+    res
+      .status(500)
+      .json({ error: 'Unable to fetch data from the external API' })
+  }
+})
+
+app.use(express.static(path.join(__dirname, '../client/build')))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'))
+})
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000')
+})
